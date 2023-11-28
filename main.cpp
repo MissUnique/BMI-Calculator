5f46fe847d06aacf7b4e9f2e4b76c157744f1dbb
MissUnique <simply.uniqu@hotmail.com>
2023-11-28 14:01:02 +0400

    Main concept

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+main () {
+    float height, weight;
+    cout << "Welcome to Azza's BMI Calculator ^_^!\n";
+    cout << "Please enter your height and weight:\n";
+    cout << "Height: ";
+    cin >> height;
+    cout << "Weight: ";
+    cin >> weight;
+}
